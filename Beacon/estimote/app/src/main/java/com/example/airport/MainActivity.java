90231b68760cf2cedc85d3e24821303c8da48350
Vinodh Krishnan <vinodhkrishnan@wsip-24-120-35-98.lv.lv.cox.net>
2016-10-23 10:59:41 -0700

    added final beacon app code

--- Beacon/estimote/app/src/main/java/com/example/airport/MainActivity.java
+++ Beacon/estimote/app/src/main/java/com/example/airport/MainActivity.java
@@ -16,6 +16,14 @@ import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 import com.estimote.sdk.SystemRequirementsChecker;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -23,7 +31,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.AsyncTask;
+public classMainActivity extends AppCompatActivity {
 
     private static final Map<String, List<String>> PLACES_BY_BEACONS;
 
@@ -72,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
                     List<String> places = placesNearBeacon(nearestBeacon);
                     // TODO: update the UI here
                     showNotification("Airport", "Nearest places: " + places);
+                    new BackgroundOperation().execute(Integer.toString(nearestBeacon.getMajor()));
                 }
             }
         });
@@ -137,4 +147,97 @@ public class MainActivity extends AppCompatActivity {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, notification);
     }
+
+    public void checkNotification(){
+        new BackgroundOperation().execute("");
+    }
+
 }
+
+class BackgroundOperation extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params){
+    //Your network connection code should be here .
+    postCall(params[0]);
+    return "";
+
+}
+
+    @Override
+    protected void onPostExecute(String result) {
+        //Print your response here .
+        Log.d("Post Response",result);
+
+    }
+
+    @Override
+    protected void onPreExecute() {}
+
+    @Override
+    protected void onProgressUpdate(Void... values) {}
+
+    public  void  postCall(String id)
+    {
+
+
+        JSONObject data = new JSONObject();
+        String time = Long.toString(System.currentTimeMillis());
+        try {
+            data.put("uuid", "100");
+            data.put("timestamp", time);
+            data.put("distance", "12");
+            data.put("beacon_id", id);
+        }
+        catch(Exception e){}
+
+
+        BufferedReader reader=null;
+
+        // Send data
+        try
+        {
+
+            // Defined URL  where to send data
+            URL url = new URL("http://shylockservice.cfapps.io/api/v1.0/pushBeaconEvent");
+
+            // Send POST data request
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestMethod("POST");
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
+            wr.write( data.toString() );
+            wr.flush();
+            wr.close();
+            os.close();
+            conn.connect();
+
+            // Get the server response
+            int HttpResult = conn.getResponseCode();
+            if (HttpResult == HttpURLConnection.HTTP_OK){
+            }
+        }
+        catch(Exception ex)
+        {
+        }
+        finally
+        {
+            try
+            {
+
+                reader.close();
+            }
+
+            catch(Exception ex) {}
+        }
+
+    }
+
+}
+
--- Beacon/estimote/app/src/main/java/com/example/airport/MyApplication.java
+++ Beacon/estimote/app/src/main/java/com/example/airport/MyApplication.java
@@ -6,13 +6,27 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
 
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.util.List;
 import java.util.UUID;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URLEncoder;
+import java.net.URL;
+import java.io.UnsupportedEncodingException;
+import java.net.URLConnection;
+import java.io.OutputStreamWriter;
+
+import org.json.JSONObject;
 
 public class MyApplication extends Application {
 
@@ -21,7 +35,6 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         beaconManager = new BeaconManager(getApplicationContext());
         beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
             @Override
@@ -31,6 +44,7 @@ public class MyApplication extends Application {
                         "Current security wait time is 15 minutes, "
                                 + "and it's a 5 minute walk from security to the gate. "
                                 + "Looks like you've got plenty of time!");
+                new BackgroundOperation1().execute("");
             }
             @Override
             public void onExitedRegion(Region region) {
@@ -64,4 +78,89 @@ public class MyApplication extends Application {
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, notification);
     }
+
+    public void checkNotification(){
+        new BackgroundOperation1().execute("");
+    }
 }
+
+
+class BackgroundOperation1 extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        //Your network connection code should be here .
+        postCall();
+        return "";
+
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        //Print your response here .
+        Log.d("Post Response", result);
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+    }
+
+    public void postCall() {
+
+
+        JSONObject data = new JSONObject();
+        try {
+            data.put("uuid", "100");
+            data.put("timestamp", "20161022");
+            data.put("distance", "12");
+            data.put("beacon_id", "101");
+        } catch (Exception e) {
+        }
+
+
+        BufferedReader reader = null;
+
+        // Send data
+        try {
+
+            // Defined URL  where to send data
+            URL url = new URL("http://shylockservice.cfapps.io/api/v1.0/pushBeaconEvent");
+
+            // Send POST data request
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestMethod("POST");
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            wr.write(data.toString());
+            wr.flush();
+            wr.close();
+            os.close();
+            conn.connect();
+
+            // Get the server response
+            int HttpResult = conn.getResponseCode();
+            if (HttpResult == HttpURLConnection.HTTP_OK) {
+            }
+        } catch (Exception ex) {
+        } finally {
+            try {
+
+                reader.close();
+            } catch (Exception ex) {
+            }
+        }
+
+    }
+}
+
